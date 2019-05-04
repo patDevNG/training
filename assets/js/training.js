23899c8472b5144becc56c1678c49dc90fc3cf3d
patDevNG <cgrleadership@gmail.com>
2019-05-04 14:20:50 +0100

    addad array

--- assets/js/training.js
+++ assets/js/training.js
@@ -21,13 +21,97 @@
 // }
 // console.log(num1);
 
-let age =10;
-if(age>18){
-    console.log('welcome');
+
+// let age =10;
+// if(age>18){
+//     console.log('welcome');
     
-}else{
-    console.log('Go away');
+// }else{
+//     console.log('Go away');
+    
+// }
+
+// if else
+
+// let age = 18;
+
+// if(age>= 18){
+//     console.log('You are an Adult');
+    
+// }else{
+//     console.log('You are too young to use this app');
+    
+// }
+
+
+
+// let age = 1000;
+
+// switch(age){
+//     case 0:
+//     console.log('age is 0');
+//     break;
+//     case 12:
+//     console.log('Hello');
+//     break
+//     case 23:
+//     console.log('you are welcome');
+//     break;
+
+//     default:
+//     console.log('No age range found');
+    
+// }
+
+//counting from 1-10
+
+// for(let i=1; i<=10;i++){
+//     console.log(i);
+    
+// }
+// let i = 2;
+// while(i<=10){
+//     console.log(i);
+    
+// }
+// let i = 1;
+
+// do{
+//     console.log(i);
+    
+// } while(i)
+
+let i =1;
+
+// while(i<=10){
+//     console.log(i);
+
+//     i++;
+// }
+
+// do{
+//     console.log(i);
+
+//     i++;
+    
+// }while(i<=10);
+
+let snacks = ['doughnut','eggroll','meatpie']
+console.log(snacks);
+
+for(let i = 0; i<=snacks.length-1; i++){
+    console.log(snacks[i]);
     
 }
 
+snacks.push('princess');
+console.log(snacks);
 
+snacks.unshift('princess');
+console.log(snacks);
+
+snacks.pop('princess');
+console.log(snacks);
+ snacks.shift();
+ console.log(snacks);
+ 
